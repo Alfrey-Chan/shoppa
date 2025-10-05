1ce61a19a8e54ff95fd3db3f16ef4db8781f0452
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2025-10-04 23:39:32 -0700

    create footer

---
+++ shoppa/Shoppa/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <p>© 2025 Shoppa Japan. Personal shopping service inspired by the spirit of omotenashi.</p>
+        </div>
+    )
+}
+
+export default Footer;
